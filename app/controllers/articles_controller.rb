365e806c6a6e3574ef514c5fbb411670d2acf4fd
Travis Rails Udemy Course <travisjansevanrensburg@gmail.com>
2019-05-24 07:36:21 +0200

    Add category show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,7 @@ class ArticlesController < ApplicationController
  end
 
  def show
+
  end
 
  def destroy
@@ -49,7 +50,7 @@ class ArticlesController < ApplicationController
  end
 
  def article_params
-  params.require(:article).permit(:title, :description)
+  params.require(:article).permit(:title, :description, category_ids: [])
  end
 
  def require_same_user
